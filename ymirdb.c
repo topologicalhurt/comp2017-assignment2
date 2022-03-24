d3788bb2468b72d475570d59abefde78ad62a301
topologicalhurt <85112999+topologicalhurt@users.noreply.github.com>
2022-03-24 15:37:13 +1100

    Update ymirdb.c

--- ymirdb.c
+++ ymirdb.c
@@ -4,6 +4,11 @@
  * <510427586>
  */
 
+/* TODOS:
+
+1) FIX INVALID ARGS PROVIDED (E.G letters instead of numbers)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,16 +18,72 @@
 
 #include "ymirdb.h"
 
-extern inline uint64_t djb2h(char * str);
-extern inline bool is_key(char * k);
-extern inline bool is_simple(char ** args, size_t nargs);
+extern inline uint64_t djb2h(const char * str);
+extern inline bool is_key(const char * k);
+extern inline bool is_simple(char ** args, const size_t nargs, const size_t start);
+extern inline bool is_num(const char * s);
+extern inline void destroy_entry(entry * e);
+extern inline void shallow_copy_entry(entry ** e1, const entry * e2);
+extern inline void dcer(entry ** e1, const entry * e2);
+extern inline void dcar(entry ** e1, const entry * e2);
 
-void destroy_entry(entry * e) {
-	free(e -> values);
-	//free(e -> next);
-	//free(e -> prev);
-	// Free remaining elements (forward and backward)
+entry * dbHead = NULL; // db header
+//entry * tail = NULL; // db tail
 
+// PROCESS KEYS THEN VALS.
+void pktv(entry ** e, char ** args, size_t nargs) {
+	*e = malloc(sizeof(struct entry));
+	memcpy((*e) -> key, args[1], strlen(args[1]));
+	(*e) -> length = (nargs -= 2);
+	(*e) -> isSimp = is_simple(args, nargs, 2);
+	size_t tmpsize = (nargs * sizeof(struct element));
+	element * tmps = malloc(tmpsize);
+	if((*e) -> isSimp) {
+		// Simple struct
+		for(size_t i = 0; i < nargs; i++) {
+			struct element tmp;
+			tmp.type = INTEGER;
+			tmp.value = atoi(args[i + 2]);
+			tmps[i] = tmp;
+		}
+		(*e) -> values = malloc(tmpsize);
+		memcpy((*e) -> values, tmps, tmpsize);
+	} else {
+		// General struct
+		
+	}
+	free(tmps);
+}
+
+void destroy_db(entry * dbh) {
+	entry * tmp;
+	while(dbh) {
+		tmp = dbh;
+		dbh = dbh -> next;
+		destroy_entry(tmp);
+	}
+}
+
+void destroy_args(char ** args, const size_t nargs) {
+	for(size_t i = 0; i < nargs; i++) {
+    free(args[i]);
+	}
+  free(args);
+}
+
+void append_db(entry ** tail, entry * e) {
+
+}
+
+void push_db(entry ** head, entry * e) {
+  // Returns: new head.
+  // Insert at front (leftmost) position. Don't mutate e.
+  entry * nn;
+	dcar(&nn, e);
+	nn -> next = *head;
+	nn -> prev = NULL;
+	if(*head) (*head) -> prev = nn;
+	*head = nn;
 }
 
 void command_bye() {
@@ -33,7 +94,24 @@ void command_help() {
 	printf("%s\n", HELP);
 }
 
-void cmd_set(entry e) {
+void cmd_set(entry * e) {
+	#if DEBUG
+		LOG_ENTRY(e);
+	#endif
+	//push_dbHead(&dbHead, e);
+}
+
+void cmd_push(entry * e) {
+	#if DEBUG
+		LOG_ENTRY(e);
+		push_db(&dbHead, e);
+		LOG_LL(dbHead);
+	#else
+		push_dbHead(&dbHead, e);
+	#endif
+}
+
+void cmd_append(entry * e) {
 	#if DEBUG
 		LOG_ENTRY(e);
 	#endif
@@ -43,46 +121,12 @@ element * cmd_get() {
 	return NULL;
 }
 
-void cmd_push(entry e) {
-	#if DEBUG
-		LOG_ENTRY(e);
-	#endif
-}
-
-void cmd_append(entry e) {
-	#if DEBUG
-		LOG_ENTRY(e);
-	#endif
-}
-
-// PROCESS KEYS THEN VALS.
-void pktv(entry * e, char ** args, size_t nargs) {
-	memcpy(e -> key, args[1], strlen(args[1]));
-	e -> isSimp = is_simple(&args[2], nargs - 1);
-	e -> length = nargs - 1;
-	element * tmps = malloc(e -> length * sizeof(struct element));
-	if(e -> isSimp) {
-		// Simple struct
-		for(int i = 2; i <= nargs; i++) {
-			struct element tmp;
-			tmp.type = INTEGER;
-			tmp.value = atoi(args[i]);
-			memcpy(&tmps[i - 2], &tmp, sizeof(struct element));
-		}
-		e -> values = malloc(e -> length * sizeof(struct element));
-		memcpy(&(e -> values[0]), &(tmps[0]), e -> length * sizeof(struct element));
-	} else {
-		// General struct
-	}
-	free(tmps);
-}
-
 int main(void) {
 
-	char line[MAX_LINE];
-	char ** args = NULL;
-	size_t nargs; // num of args (excluding command)
-	struct entry curre; // current entry
+	char line[MAX_LINE]; // Raw input
+	char ** args = NULL; // User supplied arguments
+	size_t nargs; // Num. of args (excluding command)
+	entry * curre = NULL; // Current entry
 
 	while (true) {
 		printf("> ");
@@ -90,7 +134,7 @@ int main(void) {
 		if (NULL == fgets(line, MAX_LINE, stdin)) {
 			printf("\n");
 			command_bye();
-			return 0;
+			break;
 		}
 
 		args = read_args(line, strlen(line), &nargs);
@@ -101,27 +145,27 @@ int main(void) {
 			switch(djb2h(args[0])) {
 				case SET:
 					// Check args
-					if(nargs >= 2 && is_key(args[1])) {
+					if(nargs > 2 && is_key(args[1])) {
 						pktv(&curre, args, nargs);
+						cmd_set(curre);
 					}
-					cmd_set(curre);
 					break;
 				case GET:
 					// Check args
 					break;
 				case PUSH:
 					// Check args
-					if(nargs >= 2 && is_key(args[1])) {
+					if(nargs > 2 && is_key(args[1])) {
 						pktv(&curre, args, nargs);
+						cmd_push(curre);
 					}
-					cmd_push(curre);
 					break;
 				case APPEND:
 					// Check args
-					if(nargs >= 2 && is_key(args[1])) {
+					if(nargs > 2 && is_key(args[1])) {
 						pktv(&curre, args, nargs);
+						cmd_append(curre);
 					}
-					cmd_append(curre);
 					break;
 				default:
 					// Command not recognised
@@ -129,15 +173,12 @@ int main(void) {
 					break;
 			}
 
-			// DESTRUCTOR's CALLED
-			destroy_entry(&curre);
-			for(int i = 0; i < nargs; i++) {
-				free(args[i]);
-			}
-			free(args);
+			// Call destructors for 'garbage collection.'
+			destroy_entry(curre);
+			destroy_args(args, nargs);
 		}
 	}
-	
-	free(curre);
+	// free all entries
+	destroy_db(dbHead);
 	return 0;
 }
